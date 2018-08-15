7dc96b7389133f97781373a8978b7ebbb8e87f51
root <root@PC-FIX-CESAR.localdomain>
2018-08-15 12:54:41 +0200

    squash migrations + armies tabs

--- warhammer_toolbox/home/urls.py
+++ warhammer_toolbox/home/urls.py
@@ -15,14 +15,13 @@ urlpatterns = [
     url(r'^logout/$', auth_views.logout, {'next_page': reverse_lazy('home:login')}, name='logout'),
 
     # armory
-    url(r'armory/$', views.armory, name='armory'),
-    url(r'armory/(?P<race_id>\d+)/create$', views.armory_army_create, name='armory_army_create'),
-    url(r'armory/(?P<army_id>\d+)/$', views.armory_army_details, name='armory_army_details'),
-    url(r'armory/(?P<army_id>\d+)/edit$', views.armory_army_edit, name='armory_army_edit'),
-    url(r'armory/(?P<army_id>\d+)/delete$', views.armory_army_delete, name='armory_army_delete'),
-
-    url(r'armory/(?P<army_id>\d+)/unit/(?P<role_id>\d+)/create$', views.armory_unit_create, name='armory_unit_create'),
-    url(r'armory/(?P<army_id>\d+)/unit/(?P<unit_id>\d+)/$', views.armory_unit_details, name='armory_unit_details'),
-    url(r'armory/(?P<army_id>\d+)/unit/(?P<unit_id>\d+)/edit/$', views.armory_unit_edit, name='armory_unit_edit'),
-    url(r'armory/(?P<army_id>\d+)/unit/(?P<unit_id>\d+)/delete/$', views.armory_unit_delete, name='armory_unit_delete'),
+    url(r'army/$', views.armory, name='armory'),
+    url(r'army/(?P<race_id>\d+)/create$', views.armory_army_create, name='armory_army_create'),
+    url(r'army/(?P<army_id>\d+)/$', views.armory_army_details, name='armory_army_details'),
+    url(r'army/(?P<army_id>\d+)/edit$', views.armory_army_edit, name='armory_army_edit'),
+    url(r'army/(?P<army_id>\d+)/delete$', views.armory_army_delete, name='armory_army_delete'),
+    url(r'army/(?P<army_id>\d+)/unit/(?P<role_id>\d+)/create$', views.armory_unit_create, name='armory_unit_create'),
+    url(r'army/(?P<army_id>\d+)/unit/(?P<unit_id>\d+)/$', views.armory_unit_details, name='armory_unit_details'),
+    url(r'army/(?P<army_id>\d+)/unit/(?P<unit_id>\d+)/edit/$', views.armory_unit_edit, name='armory_unit_edit'),
+    url(r'army/(?P<army_id>\d+)/unit/(?P<unit_id>\d+)/delete/$', views.armory_unit_delete, name='armory_unit_delete'),
 ]
